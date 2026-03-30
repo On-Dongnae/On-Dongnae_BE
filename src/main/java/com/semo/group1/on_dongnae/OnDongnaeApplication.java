a174ade150a31afd8094d19a7396585cd7becb8d
cylin0201 <cylin0201@naver.com>
2026-03-30 12:03:08 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/semo/group1/on_dongnae/OnDongnaeApplication.java
@@ -0,0 +1,13 @@
+package com.semo.group1.on_dongnae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnDongnaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnDongnaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semo/group1/on_dongnae/OnDongnaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semo.group1.on_dongnae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnDongnaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
